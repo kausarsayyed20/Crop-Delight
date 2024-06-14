468c993917d0621396c636d1f1f7cce21c56c7c1
ANN ELZA JOSE <ANN ELZA JOSE@LAPTOP-86MJFK09>
2024-06-14 14:36:32 -0400

    Added product form page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Register from '../src/Components/Register/RegisterPage';
-import Login from './Components/Login/LoginPage';
-import Home from './Components/Home/HomePage'; // Create a simple Home component
+// import Register from './Components/Register/RegisterPage';
+// import Login from './Components/Login/LoginPage';
+// import Home from './Components/Home/HomePage';
+import ProductForm from './Components/Product/ProductForm'; // Import the ProductForm component
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Routes>
-          <Route path="/" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
+        {/* <Routes> */}
+          {/* <Route path="/" element={<Home />} /> Set Home as the default route */}
+          {/* <Route path="/register" element={<Register />} /> */}
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/add-product" element={<ProductForm />} /> Add the route for ProductForm */}
+        {/* </Routes> */}
+        <ProductForm />
       </div>
     </Router>
   );
